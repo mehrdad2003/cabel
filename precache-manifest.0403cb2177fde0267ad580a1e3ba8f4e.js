525ed166c64cf856d7064101d27b22275ac6d455
mehrdadzare <62208360+mehrdad2003@users.noreply.github.com>
2021-07-04 16:51:51 +0430

    Add files via upload

---
+++ precache-manifest.0403cb2177fde0267ad580a1e3ba8f4e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adc647b66411a891036ccc53e4d4f79",
+    "url": "/cabel/index.html"
+  },
+  {
+    "revision": "6fec8f662f780a235a83",
+    "url": "/cabel/static/css/main.325fe7f5.chunk.css"
+  },
+  {
+    "revision": "3f6b0b01d1c1d7838295",
+    "url": "/cabel/static/js/2.f494b69f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cabel/static/js/2.f494b69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fec8f662f780a235a83",
+    "url": "/cabel/static/js/main.6773e5cf.chunk.js"
+  },
+  {
+    "revision": "bc0e13faf7443a20d161",
+    "url": "/cabel/static/js/runtime-main.7ad7c6ed.js"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/cabel/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "ea85e7aa8379f1549481b5fa40cc3c6a",
+    "url": "/cabel/static/media/svg-2.ea85e7aa.svg"
+  },
+  {
+    "revision": "b953e5b996d894979a122f36ea4ea945",
+    "url": "/cabel/static/media/svg-3.b953e5b9.svg"
+  },
+  {
+    "revision": "a621f5b69f7fdcd62fd0ace7bcf69447",
+    "url": "/cabel/static/media/video.a621f5b6.mp4"
+  }
+]);
