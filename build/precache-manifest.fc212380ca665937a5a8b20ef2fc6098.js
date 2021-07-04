b973926224045efcfb2c54d4384b795a9937ef48
unknown <mehrdadzare2003@gmail.com>
2021-07-04 20:25:07 +0430

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.fc212380ca665937a5a8b20ef2fc6098.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad039fe39725b988b4b7fefb4004be6",
+    "url": "/cabel/index.html"
+  },
+  {
+    "revision": "d56cb221ea5e18d105ea",
+    "url": "/cabel/static/css/main.f4fd0b83.chunk.css"
+  },
+  {
+    "revision": "3f6b0b01d1c1d7838295",
+    "url": "/cabel/static/js/2.f494b69f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cabel/static/js/2.f494b69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56cb221ea5e18d105ea",
+    "url": "/cabel/static/js/main.5b2f6588.chunk.js"
+  },
+  {
+    "revision": "bc0e13faf7443a20d161",
+    "url": "/cabel/static/js/runtime-main.7ad7c6ed.js"
+  },
+  {
+    "revision": "bab5e043e4fc4bcce51699f943003c85",
+    "url": "/cabel/static/media/IRANSansWeb.bab5e043.ttf"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/cabel/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "ea85e7aa8379f1549481b5fa40cc3c6a",
+    "url": "/cabel/static/media/svg-2.ea85e7aa.svg"
+  },
+  {
+    "revision": "b953e5b996d894979a122f36ea4ea945",
+    "url": "/cabel/static/media/svg-3.b953e5b9.svg"
+  },
+  {
+    "revision": "2c8a532c3e15c1651a8ae49ec5bfeb79",
+    "url": "/cabel/static/media/video.2c8a532c.mp4"
+  }
+]);
